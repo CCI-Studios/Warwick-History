0f82ec45cbb9baa48efaf6f8c7d80a2005f58723
Jonathan Bennett <jbennett@ccistudios.com>
2010-07-27 17:09:48 -0400

    Initial setup of IE6 Redirect plugin

---
+++ plugins/system/ie6Redirect.php
@@ -0,0 +1,18 @@
+<?php
+defined('_JEXEC') or die; 
+
+jimport('joomla.plugin.plugin');
+jimport('joomla.environment.browser');
+
+class plgSystemIE6Redirect extends JPlugin {
+
+	//public function __construct(&$subject, &$config) {
+	//}
+
+	public function onAfterRoute() {
+		
+	}
+	
+	public function onAfterRender() {
+	}
+}
