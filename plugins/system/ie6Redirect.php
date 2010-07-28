7fa020c46839f5ffecb0638758d66dac45934e56
Jonathan Bennett <jbennett@ccistudios.com>
2010-07-28 13:47:39 -0400

    Completed IE6 redirect plugin

--- plugins/system/ie6Redirect.php
+++ plugins/system/ie6Redirect.php
@@ -6,13 +6,47 @@ jimport('joomla.environment.browser');
 
 class plgSystemIE6Redirect extends JPlugin {
 
-	//public function __construct(&$subject, &$config) {
-	//}
+	public function __construct(&$subject, $config) {
+		parent::__construct($subject, $config);
+	}
 
 	public function onAfterRoute() {
-		
+		$app =& JFactory::getApplication();
+		$tmpl = JRequest::getVar('tmpl', 'cmd');
+	
+		if ($tmpl != 'ie6' && $app->isSite() && $this->params->get('redirect') == 'redirect') {
+			$browser =& JBrowser::getInstance();
+			if (stripos($browser->getAgentString(), 'chrome')) {
+			//if ($browser->getBrowser() == 'msie' && $browser->getMajor() == '7') {
+				$url = http_build_query(JRequest::get('get'));
+				if (strlen($url) > 0) {
+					$url .= '&tmpl=ie6';
+				} else {
+					$url .= 'tmpl=ie6';
+				}
+				
+				$app->redirect('index.php?'.$url);			
+			}
+		}
 	}
 	
 	public function onAfterRender() {
+		$app =& JFactory::getApplication();
+	
+		if ($app->isSite() && $this->params->get('redirect') == 'warn') {
+			$browser =& JBrowser::getInstance();
+			if (stripos($browser->getAgentString(), 'chrome')) {
+			//if ($browser->getBrowser() == 'msie' && $browser->getMajor() == '7') {
+				$body = JResponse::getBody();
+				
+				$pattern = "/(<body .*?>)/i";
+				$replace = "$1\n<div class=\"ie6warn\"><p>You are currently using an unsupported browser and may experience issues browsing the site. Click <a href=\"/index.php?tmpl=ie6\">here</a> for more information.</p></div>";
+				
+				$body = preg_replace($pattern, $replace, $body, 1);
+				
+				
+				JResponse::setBody($body);
+			}
+		}
 	}
 }
--- templates/ccistudios/ie6.php
+++ templates/ccistudios/ie6.php
@@ -5,21 +5,89 @@
 	
 	<style type="text/css">
 	.outer {
-		width: 400px;
+		width: 800px;
 		margin: 30px auto 0;
 		text-align: center;
 	}
 	.inner {
-		text-align: left;
+		width: 600px;
+		margin: 50px auto;
+		
+		font-size: 15px;
+		line-height: 1.4em;
+		font-family: "Century Gothic";
+		color:  red;
+	}
+	.image {
+		float: left;
+		width: 25%;
+		text-align: center;
+	}
+	.clr {
+		clear: both;
+	}
+	a {
+		text-decoration: none;
+		color: black;
 	}
 	</style>
 </head>
 
 <body>
+	<script type="text/javascript">
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-XXXXXXXX-X']);
+	  _gaq.push(['_trackPageview']);
+	
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+	</script>
+
 	<div class="outer">
 		<div class="inner">
-		ie6
+			<?php 
+			$conf =& JFactory::getConfig();
+			$sitename = $conf->getValue('config.sitename');
+			?>
+			<p class="center">
+				<img src="/templates/<?php echo $this->template;?>/images/logo.png" alt="<?php echo $sitename; ?>" />
+			</p>
+			<p>Thank you for visiting <?php echo $sitename; ?>!</p>
+			<p>You are currently viewing our website with the Internet Explorer 6 web browser.
+			Being that this is an old browser, you will have a hard time viewing our site as it
+			was meant to be seen!</p>
+			<p>Click on any of the links below to install a current browser or to update
+			the Internet Explorer browser itself.</p>
+			<p>Thanks and we hope to see you back soon!</p>
 		</div>
+		<div class="image">
+			<a href="" target="_blank">
+				<img src="/templates/<?php echo $this->template;?>/images/ie6/chrome.jpg" alt="Google Chrome" /><br/>
+				Chrome
+			</a>
+		</div>
+		<div class="image">
+			<a href="" target="_blank">
+				<img src="/templates/<?php echo $this->template;?>/images/ie6/firefox.jpg" alt="Mozilla Firefox" /><br/>
+				Firefox
+			</a>
+		</div>
+		<div class="image">
+			<a href="" target="_blank">
+				<img src="/templates/<?php echo $this->template;?>/images/ie6/safari.jpg" alt="Apple Safari" /><br/>
+				Safari
+			</a>
+		</div>
+		<div class="image">
+			<a href="" target="_blank">
+				<img src="/templates/<?php echo $this->template;?>/images/ie6/ie.jpg" alt="Microsoft Internet Explorer" /><br/>
+				Internet Explorer
+			</a>
+		</div>
+		<div class="clr"></div>
 	</div>
 </body>
 </html>
