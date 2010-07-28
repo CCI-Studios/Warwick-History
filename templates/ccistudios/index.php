51e1585aa9833f0b730c043ef59001db6a566408
Jonathan Bennett <jbennett@ccistudios.com>
2010-07-28 13:49:51 -0400

    Added some styling of IE6 pages/content

--- templates/ccistudios/index.php
+++ templates/ccistudios/index.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <?php
-$menu = 'home';
-$testing = "true";s
+$menu = JSite::getMenu();
+if ($menu)
+    $menu = $menu->getActive();
+if ($menu)
+    $menu = $menu->alias;
+$testing = "true";
 
 ?>
 <html>
@@ -14,16 +18,32 @@ $testing = "true";s
 <?php endif; ?>
 
 
+<?php if ($testing): ?>
 	<link rel="stylesheet" type="text/css" href="/templates/<?php echo $this->template ?>/css/reset.css" />
+	<link rel="stylesheet" type="text/css" href="/templates/<?php echo $this->template ?>/css/grid.css" />
 	<link rel="stylesheet" type="text/css" href="/templates/<?php echo $this->template ?>/css/forms.css" />
 	<link rel="stylesheet" type="text/css" href="/templates/<?php echo $this->template ?>/css/layout.css" />
 	<link rel="stylesheet" type="text/css" href="/templates/<?php echo $this->template ?>/css/template.css" />
 	<link rel="stylesheet" type="text/css" href="/templates/<?php echo $this->template ?>/css/style.css" />
+	<link rel="stylesheet" type="text/css" href="/templates/<?php echo $this->template ?>/css/editorStyles.css" />
+<?php else: ?>
+	<link rel="stylesheet" type="text/css" href="/templates/<?php echo $this->template ?>/css/template.css" />
+<?php endif; ?>
 </head>
 
-<body>
+<body class="<?php echo $menu; ?>">
 <?php if (!$testing): ?>
-GOOGLE ANALYTICS
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-XXXXXXXX-X']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
 <?php endif; ?>
 index.php
 
