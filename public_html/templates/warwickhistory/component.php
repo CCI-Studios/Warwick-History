0b0c7787e54b690c346c844db36207837c74d5ed
JBennett <jbennett@ccistudios.com>
2010-09-09 17:00:54 -0400

    Added extra stylesheets to component output template

--- public_html/templates/warwickhistory/component.php
+++ public_html/templates/warwickhistory/component.php
@@ -5,6 +5,7 @@
 
 	<link rel="stylesheet" type="text/css" href="/templates/warwickhistory/css/reset.css" />
 	<link rel="stylesheet" type="text/css" href="/templates/warwickhistory/css/text.css" />
+	<link rel="stylesheet" type="text/css" href="/templates/warwickhistory/css/styles.css" />
 	<link rel="stylesheet" type="text/css" href="/templates/warwickhistory/css/print.css" />
 </head>
 
